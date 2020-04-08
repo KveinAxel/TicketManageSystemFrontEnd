13bd7f3cad6c8655865dbcbfbb4fdd1c822f4c56
Kevin Axel <2438679008@qq.com>
2020-04-08 08:50:29 +0800

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,8 @@ export const constantRouterMap = [
     {path: '/login', component: () => import('@/views/login/index'), hidden: true},
     {path: '/register', component: () => import('@/views/login/register'), hidden: true},
     {path: '/404', component: () => import('@/views/404'), hidden: true},
-    {path: '/', component: () => import('@/views/home'), hidden: true},
+    {path: '/user', component: () => import('@/views/user'), hidden: true},
+    {path: '/admin', component: () => import('@/views/admin'), hidden: true},
     {path: '*', redirect: '/404', hidden: true}
 ];
 
